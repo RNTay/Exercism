7898616d1cbe9342219138d8273589950ed48964
RNTay <81459529+RNTay@users.noreply.github.com>
2021-06-04 17:19:57 +0800

    removed unnecessary code
    but it still functions the exact same

--- Python/high_scores.py
+++ Python/high_scores.py
@@ -9,8 +9,4 @@ def personal_best(scores):
 
 
 def personal_top_three(scores):
-    if len(scores) >= 3:
-        top_three = sorted(scores, reverse=True)[:3].copy()
-    else:
-        top_three = sorted(scores, reverse=True).copy()
-    return top_three
+    return sorted(scores, reverse=True)[:3]
