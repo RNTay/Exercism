a898f5a3d20155689c93074388bf4ca1347aa439
RNTay <81459529+RNTay@users.noreply.github.com>
2021-06-09 00:41:33 +0800

    only have store matrix once instead of twice

--- Python/matrix.py
+++ Python/matrix.py
@@ -2,11 +2,10 @@
 
 class Matrix:
     def __init__(self, matrix_string):
-        self.rows = [[int(a) for a in b.split()] for b in matrix_string.split('\n')]
-        self.columns = [list(_) for _ in list(zip(*self.rows))]
+        self.matrix = [[int(a) for a in b.split()] for b in matrix_string.splitlines()]
 
     def row(self, index):
-        return self.rows[index - 1]
+        return self.matrix[index - 1]
 
     def column(self, index):
-        return self.columns[index - 1]
+        return [m[index - 1] for m in self.matrix]
