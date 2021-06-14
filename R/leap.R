d71bf4ef9a62a9fa351d638bf1ea65e19ae0c996
RNTay <81459529+RNTay@users.noreply.github.com>
2021-06-14 15:31:00 +0800

    reduced number of if-else needed

--- R/leap.R
+++ R/leap.R
@@ -1,15 +1,4 @@
 leap <- function(year) {
-  if (year %% 4 == 0) {
-    if (year %% 100 == 0) {
-      if (year %% 400 == 0) {
-        return(TRUE)
-      } else {
-        return(FALSE)
-      }
-    } else {
-      return(TRUE)
-    }
-  } else {
-    return(FALSE)
-  }
+  return((year %% 400 == 0) | ((year %% 4 == 0) & 
+                            (year %% 100 != 0)))
 }
