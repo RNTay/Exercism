ece85ea7f4713f512b66233ba5b8ed9fb8cde6f9
RNTay <81459529+RNTay@users.noreply.github.com>
2021-06-14 13:57:05 +0800

    unnecessary blank line

--- Python/triangle.py
+++ Python/triangle.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-
 def is_triangle(sides):
     a, b, c = sides[0], sides[1], sides[2]
     return ((a > 0) and (b > 0) and (c > 0)) and ((a + b > c) and (a + c > b) and (b + c > a))
